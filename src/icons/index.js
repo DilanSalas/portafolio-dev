7b00fdf4a7415016167725d24ad343582cd4629b
Dilan <dilansalas200@gmail.com>
2025-01-30 17:00:34 -0600

    Translate Lenguaje to spanish

--- src/icons/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Brain } from './Brain.astro';
-export { default as Code } from './Code.astro';
-export { default as Email } from './Email.astro';
-export { default as GitHub } from './GitHub.astro';
-export { default as Linke } from './Linke.astro';
-export { default as Phone } from './Phone.astro';  
-export { default as Twitter } from './Twitter.astro';
